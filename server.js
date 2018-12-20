b68cd0cdb0f4d56e73fd1d96cf084539430b6b1a
Kiril Stas <vesground@gmail.com>
2018-12-20 20:36:44 +0200

    Removed builds from git, separeted builds and statics in server

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var port = 9000;
 var express = require("express");
 
 var app = express();
-app.use('/', express.static(__dirname + '/build/statics'));
+app.use('/statics/', express.static(__dirname + '/dist/statics'));
+app.use('/', express.static(__dirname + '/dist/builds'));
 app.listen(port, host);
 
 console.log('Running server at ' + host + ':' + port + '/');
